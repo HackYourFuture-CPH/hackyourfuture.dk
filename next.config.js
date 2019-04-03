9afc535b2017fc19e2b7254450cb90dedfe55cce
Ahmad Magdy <magdy.a@hotmail.com>
2019-04-03 10:58:28 +0200

    Update webpack config changing output.filename

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = {
   webpack: (config, { defaultLoaders }) => {
+    // Change output file name format (ex: main.js-{hash}.bundle.js)
+    config.output.filename = '[name]-[hash].bundle.js'
     config.module.rules.push({
       test: /\.scss$/,
       use: [
@@ -17,7 +19,7 @@ module.exports = {
     if (config.mode === 'production') {
       config.plugins.push(
         new CompressionPlugin(),
-      ); 
+      );
     }
     
     return config
